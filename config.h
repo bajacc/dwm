f13cfc254314a6ce89bafc2cb781e31244689d3b
bajacc <baptiste.jacquemot@gmail.com>
2024-06-01 12:03:37 +0200

    new keybinding

--- config.h
+++ config.h
@@ -57,6 +57,7 @@ static const Layout layouts[] = {
 
 /* key definitions */
 #define MODKEY Mod4Mask
+#define OPTIONKEY Mod1Mask
 #define TAGKEYS(KEY,TAG) \
 	{ MODKEY,                       KEY,      view,           {.ui = 1 << TAG} }, \
 	{ MODKEY|ControlMask,           KEY,      toggleview,     {.ui = 1 << TAG} }, \
@@ -73,6 +74,7 @@ static const char *termcmd[]  = { "st", NULL };
 
 #include <X11/XF86keysym.h>
 static Key keys[] = {
+	
 	/* modifier                     key        function        argument */
 	{ MODKEY,                       XK_d,      spawn,          {.v = dmenucmd } },
 	{ MODKEY,         			    XK_Return, spawn,          SHCMD("$TERMINAL") },
@@ -111,6 +113,9 @@ static Key keys[] = {
 	{ 0, XF86XK_AudioMute,			spawn,		SHCMD("pamixer -t; kill -44 $(pidof dwmblocks)") },
 	{ 0, XF86XK_MonBrightnessUp,	spawn,		SHCMD("xbacklight -inc 10; kill -45 $(pidof dwmblocks)") },
 	{ 0, XF86XK_MonBrightnessDown,	spawn,		SHCMD("xbacklight -dec 10; kill -45 $(pidof dwmblocks)") },
+
+	{ OPTIONKEY, XK_Left,	spawn,		SHCMD("xdotool key Home") },
+	{ OPTIONKEY, XK_Right,	spawn,		SHCMD("xdotool key End") },
 
 	{ 0,				XK_Print,	spawn,		SHCMD("maim pic-full-$(date '+%y%m%d-%H%M-%S').png") },
 	{ MODKEY,			XK_Print,	spawn,		SHCMD("dmenupics") },
