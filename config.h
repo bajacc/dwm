3f4aaae8bcedaecbe057627f36783990e77e15f1
bajacc <baptiste.jacquemot@orange.fr>
2024-10-04 14:47:58 +0200

    set win+c win+v as universal copy paste command

--- config.h
+++ config.h
@@ -79,6 +79,8 @@ static Key keys[] = {
 	{ MODKEY,                       XK_d,      spawn,          {.v = dmenucmd } },
 	{ MODKEY,         			    XK_Return, spawn,          SHCMD("$TERMINAL") },
 	{ MODKEY,         			    XK_w,	   spawn,          SHCMD("$BROWSER") },
+	{ MODKEY,                       XK_c,      spawn,          SHCMD("xdotool keyup C key --clearmodifiers ctrl+Insert") },
+	{ MODKEY,                       XK_v,      spawn,          SHCMD("xdotool keyup V key --clearmodifiers shift+Insert") },
 	{ MODKEY,                       XK_b,      togglebar,      {0} },
 	{ MODKEY,                       XK_j,      focusstack,     {.i = +1 } },
 	{ MODKEY,                       XK_k,      focusstack,     {.i = -1 } },
